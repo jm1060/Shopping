1ac28db69df25e1f48dad2de35c9feae0b33c575
James <jm1060@rit.edu>
2024-11-22 16:04:51 -0500

    made a cart dictionary

--- shopping.py
+++ shopping.py
@@ -2,9 +2,16 @@ import random
 def shopping(amount):
     items={'jackets':25.99, 'sweater':15.50, 't-shirts':13.69, 'jeans': '16.49', 'sweatpants':'20.99','sneakers':14.79, 
     'pencils':0.99, 'pens': 1.99, 'eraser':1.59, 'notebooks':2.99, 'binders':5.99, 'folders': 2.59, 'markers':'2.00'}
+    cart=dict()
     store_item = input("What would you like to buy?")
     while not store_item in items.keys():
         print("This item is not in the store. Try again")
+    quantity = input("How many would you like to buy?")
+    if(store_item in cart):
+        cart[store_item]+=quantity
+    else:
+        cart[store_item]=quantity
+    
     
 
 
