2a82167998f6d9ec4663930973d4349d8e90f77b
James <jm1060@rit.edu>
2024-11-23 11:22:09 -0500

    Fixed some issues in the dictionary

--- shopping.py
+++ shopping.py
@@ -1,7 +1,7 @@
 import random
 def shopping(amount):
-    items={'jackets':25.99, 'sweater':15.50, 't-shirts':13.69, 'jeans': '16.49', 'sweatpants':'20.99','sneakers':14.79, 
-    'pencils':0.99, 'pens': 1.99, 'eraser':1.59, 'notebooks':2.99, 'binders':5.99, 'folders': 2.59, 'markers':'2.00'}
+    items={'jackets':25.99, 'sweater':15.50, 't-shirts':13.69, 'jeans': 16.49, 'sweatpants':20.99,'sneakers':14.79, 
+    'pencils':0.99, 'pens': 1.99, 'eraser':1.59, 'notebooks':2.99, 'binders':5.99, 'folders': 2.59, 'markers':2.00}
     cart=dict()
     total = 0
     store_item = input("What would you like to buy? Enter item (or type 'Done' to finish):").lower()
@@ -21,7 +21,7 @@ def shopping(amount):
             cart[store_item] += quantity 
         else: 
             cart[store_item] = quantity 
-        total += items[store_item] * quantity 
+        total += items[store_item] * float(quantity) 
         store_item = input("Is there anything else you would like to buy? (type 'Done' to finish): ").lower()
     print("Seems like you are ready to check out. Let's check the total")
     if(total > amount):
